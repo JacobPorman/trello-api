84bf5b906a293e601bd2979789a7309639065f6f
JacobPorman <nguyenhungtnhtoan1920@gmail.com>
2025-11-03 18:18:24 +0700

    Init Code base

---
+++ src/utils/sorts.js
@@ -0,0 +1,24 @@
+/**
+ * ---
+ * Order an array of objects based on another array & return new Ordered Array
+ * The original array will not be modified.
+ * ---
+ * @param {*} originalArray
+ * @param {*} orderArray
+ * @param {*} key = Key to order
+ * @return new Ordered Array
+ *
+ * How it works:
+ * Use indexOf to determine each element’s position in orderArray, then sort originalArray based on those positions using JavaScript’s sort function.
+ */
+
+export const mapOrder = (originalArray, orderArray, key) => {
+  if (!originalArray || !orderArray || !key) return []
+
+  const clonedArray = [...originalArray]
+  const orderedArray = clonedArray.sort((a, b) => {
+    return orderArray.indexOf(a[key]) - orderArray.indexOf(b[key])
+  })
+
+  return orderedArray
+}
