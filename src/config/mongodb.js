657116b05f2267578fff878bb8a22a318e549e7c
JacobPorman <nguyenhungtnhtoan1920@gmail.com>
2025-11-10 17:33:44 +0700

    Connect MongoDB and Env variables

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,5 +1,41 @@
-/**
- * Updated by trungquandev.com's author on August 17 2023
- * YouTube: https://youtube.com/@trungquandev
- * "A bit of fragrance clings to the hand that gives flowers!"
- */
+
+
+// eslint-disable-next-line no-unused-vars
+
+
+import { MongoClient, ServerApiVersion } from 'mongodb'
+import { env } from '~/config/environment'
+
+
+// Initialize an object trelloDatabaseInstance with a value of null initially because the connection has not been connected yet.
+// Is Database on Atlas
+let trelloDatabaseInstance = null
+
+// Create a new MongoClient instance to connect to the MongoDB server using the provided URI and options.
+const mongoClientInstance = new MongoClient(env.MONGODB_URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+})
+
+// Connect to MongoDB
+export const CONNECT_DB = async () => {
+  // Connect to MongoDB Atlas with URI in mongoClientInstance
+  await mongoClientInstance.connect()
+
+  // mongoClientInstance.db() method is used to get a reference to a specific database by its name.
+  trelloDatabaseInstance = mongoClientInstance.db(env.DATABASE_NAME)
+}
+
+// This function has a mission to export the trelloDatabaseInstance object after the connection is successful.
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Database not connected yet. Please CONNECT_DB first.')
+  return trelloDatabaseInstance
+}
+
+// Close the database connection
+export const CLOSE_DB = async () => {
+  await mongoClientInstance.close()
+}
