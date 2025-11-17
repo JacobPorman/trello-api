06e994925d868838a7bf1d2121d13a9573751085
JacobPorman <nguyenhungtnhtoan1920@gmail.com>
2025-11-17 19:32:51 +0700

    create model and validate schema

--- src/services/boardService.js
+++ src/services/boardService.js
@@ -1,5 +1,6 @@
 /* eslint-disable no-useless-catch */
 import { slugify } from '~/utils/formatters'
+import { boardModel } from '~/models/boardModel'
 
 const createNew = async (reqBody) => {
   try {
@@ -8,9 +9,14 @@ const createNew = async (reqBody) => {
       slug: slugify(reqBody.title)
     }
 
-    // Return the newly created board object
-    return newBoard
+    // Call Model layer to save the document of collection in database
+    const createdBoard = await boardModel.createNew(newBoard)
 
+    // Get board document after call Model layer to create
+    const getNewBoard = await boardModel.findOneById(createdBoard.insertedId)
+
+    // Return the newly created board object to Controller layer
+    return getNewBoard
   } catch (error) {
     throw error
   }
