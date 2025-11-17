96d1a78007ef67ed35fc9bb93aa0d0c464dcd77e
JacobPorman <nguyenhungtnhtoan1920@gmail.com>
2025-11-17 22:56:21 +0700

    Api get detail of board

--- src/controllers/boardController.js
+++ src/controllers/boardController.js
@@ -15,6 +15,21 @@ const createNew = async (req, res, next ) => {
   }
 }
 
+const getDetails = async (req, res, next ) => {
+  try {
+    const boardId = req.params.id
+
+    // Routing data to Service layer to handle business logic
+    const board = await boardService.getDetails(boardId)
+
+    res.status(StatusCodes.OK).json(board)
+
+  } catch (error) {
+    next(error)
+  }
+}
+
 export const boardController = {
-  createNew
+  createNew,
+  getDetails
 }
